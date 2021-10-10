27299636a8bf62e2603aad09947127af53d5384c
xiehengjian <846188037@qq.com>
2021-10-10 23:28:49 +0800

    fix

--- Http.go
+++ Http.go
@@ -6,27 +6,27 @@ import (
 	"net/http"
 )
 
-func Get(url string,header map[string]string,params interface{})*http.Response{
+func Get(url string, header map[string]string, params interface{}) *http.Response {
 	//创建client
 	client := http.Client{}
 	//处理data
 	bytesData, _ := json.Marshal(params)
-	request, _ := http.NewRequest("GET", url,bytes.NewReader(bytesData))
-	for key,value :=range header{
-		request.Header.Set(key,value)
+	request, _ := http.NewRequest("GET", url, bytes.NewReader(bytesData))
+	for key, value := range header {
+		request.Header.Set(key, value)
 	}
 	response, _ := client.Do(request)
 	return response
 }
 
-func Post(url string,header map[string]string,data interface{})*http.Response{
+func Post(url string, header map[string]string, data interface{}) *http.Response {
 	//创建client
 	client := http.Client{}
 	//处理data
 	bytesData, _ := json.Marshal(data)
-	request, _ := http.NewRequest("GET", url,bytes.NewReader(bytesData))
-	for key,value :=range header{
-		request.Header.Set(key,value)
+	request, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData))
+	for key, value := range header {
+		request.Header.Set(key, value)
 	}
 	response, _ := client.Do(request)
 	return response
