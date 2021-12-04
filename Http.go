d42733f61053857a2947b5234c543fa8b7e761ee
xiehengjian <40929721+xiehengjian@users.noreply.github.com>
2021-12-04 10:17:58 +0800

    Update Http.go

--- Http.go
+++ Http.go
@@ -22,9 +22,12 @@ func Get(url string, header map[string]string, params interface{}) *http.Respons
 func Post(url string, header map[string]string, data interface{}) *http.Response {
 	//创建client
 	client := http.Client{}
-	//处理data
-	bytesData, _ := json.Marshal(data)
-	request, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData))
+	request,_:=http.NewRequest("POST",url,nil)
+	if data!=nil{
+		//处理data
+		bytesData, _ := json.Marshal(data)
+		request, _ = http.NewRequest("POST", url, bytes.NewReader(bytesData))
+	}
 	for key, value := range header {
 		request.Header.Set(key, value)
 	}
