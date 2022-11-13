fadfe6e8b5c3247b2a409591b4d271734b98ae56
xiehengjian <846188037@qq.com>
2022-11-13 03:36:48 +0000

    PUT

--- Http.go
+++ Http.go
@@ -52,3 +52,30 @@ func Post(url string, header map[string]string, data interface{}, res interface{
 	}
 	return response, err
 }
+
+func Put(url string, header map[string]string, data interface{}, res interface{}) (*http.Response, error) {
+	//创建client
+	client := http.Client{}
+	request, _ := http.NewRequest("PUT", url, nil)
+	if data != nil {
+		//处理data
+		bytesData, _ := json.Marshal(data)
+		request, _ = http.NewRequest("PUT", url, bytes.NewReader(bytesData))
+	}
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return response, err
+	}
+	if res != nil {
+		// copy body
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(response.Body)
+		response.Body.Close()
+		response.Body = ioutil.NopCloser(buf)
+		err = json.NewDecoder(buf).Decode(res)
+	}
+	return response, err
+}
